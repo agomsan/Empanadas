6dd5af536760d7762c438886dad8766dc5474018
unknown <agomsan@icloud.com>
2024-05-26 22:08:41 +0200

    Initial

---
+++ main.js
@@ -0,0 +1,6 @@
+export const fn = (x, y, z) => {
+   // Cuerpo de la función: Completa la lógica de la función aquí.
+};
+const NumberEmpanadas = x + y + z;
+
+if  (x < 0 || y < 0 || z < 0) console.log ("No se aceptan numeros inferiores a 0") 
